8a5845bc51984e596f0e2389b3d1f2aa3f5f5ce7
kentarxd <57211679+kentarxd@users.noreply.github.com>
2019-10-31 09:41:17 +0100

    Dodaj pliki projektów.

---
+++ App3/App3/Models/HomeMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        Gazetki,
+        O_nas
+
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
